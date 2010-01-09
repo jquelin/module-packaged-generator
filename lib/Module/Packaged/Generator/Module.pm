ba5e124be1d10a2e18f36623d2d26b4f01cf7aad
Jérôme Quelin <jquelin@gmail.com>
2010-01-09 12:00:25 +0100

    providing dist lazy builder

--- lib/Module/Packaged/Generator/Module.pm
+++ lib/Module/Packaged/Generator/Module.pm
@@ -5,13 +5,38 @@ use warnings;
 package Module::Packaged::Generator::Module;
 # ABSTRACT: a class representing a perl module
 
+use File::HomeDir qw{ my_home };
 use Moose;
 use MooseX::Has::Sugar;
+use Parse::CPAN::Packages;
+use Path::Class;
 
-has name    => ( ro, isa=>'Str', required );
-has version => ( ro, isa=>'Maybe[Str]' );
-has dist    => ( ro, isa=>'Str', lazy_build );
-has pkgname => ( ro, isa=>'Str', required );
+
+# -- attributes
+
+has name    => ( ro, isa=>'Str', required          );
+has version => ( ro, isa=>'Maybe[Str]'             );
+has dist    => ( ro, isa=>'Maybe[Str]', lazy_build );
+has pkgname => ( ro, isa=>'Str', required          );
+
+
+# -- initializers & builders
+
+{
+    my $pkgfile = file( my_home(), '.cpanplus', '02packages.details.txt.gz' );
+    if ( -f $pkgfile ) {
+        my $cpan = Parse::CPAN::Packages->new("$pkgfile");
+        *_build_dist = sub {
+            my $self = shift;
+            my $pkg = $cpan->package( $self->name );
+            return unless $pkg;
+            return $pkg->distribution->dist;
+        }
+    } else {
+        warn "couldn't find a cpanplus index in $pkgfile\n";
+        *_build_dist = sub { return };
+    }
+}
 
 1;
 __END__
