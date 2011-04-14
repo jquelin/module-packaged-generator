530b19f3928e36fcedbfe60d09c1df6985c241c9
Jérôme Quelin <jquelin@gmail.com>
2011-04-14 11:20:40 +0200

    new distribution driver: mageia

---
+++ lib/Module/Packaged/Generator/Distribution/Mageia.pm
@@ -0,0 +1,29 @@
+use 5.008;
+use strict;
+use warnings;
+
+package Module::Packaged::Generator::Distribution::Mageia;
+# ABSTRACT: mageia driver to fetch available modules
+
+use Moose;
+use Path::Class;
+
+extends 'Module::Packaged::Generator::Distribution::Mandriva';
+
+
+# -- public methods
+
+sub match {
+    my $mgarel = file( '/etc/mageia-release' );
+    return unless -f $mgarel;
+    my $content = $mgarel->slurp;
+    return ( $content =~ /mageia/i );
+}
+
+1;
+__END__
+
+=head1 DESCRIPTION
+
+This module is the L<Module::Packaged::Generator::Distribution> driver
+for Mageia.
