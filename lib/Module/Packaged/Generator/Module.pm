b5dd24a5c31f76955e8ff6b047532d28048f485f
Jérôme Quelin <jquelin@gmail.com>
2011-04-14 11:05:12 +0200

    cosmetic updates

--- lib/Module/Packaged/Generator/Module.pm
+++ lib/Module/Packaged/Generator/Module.pm
@@ -31,15 +31,15 @@ L<CPANPLUS> work directory. It will be eg C<Foo-Bar>.
 =attr pkgname
 
 This is the name of the package holding this module in the Linux
-distribution. Chances are that it looks like C<perl-Foo-Bar> on
-Mandriva, C<libfoo-bar-perl> on Debian, etc. It's required.
+distribution. Chances are that it looks like C<perl-Foo-Bar> on Mageia
+or Mandriva, C<libfoo-bar-perl> on Debian, etc. It's required.
 
 =cut
 
-has name    => ( ro, isa=>'Str', required          );
+has name    => ( ro, isa=>'Str',        required   );
 has version => ( ro, isa=>'Maybe[Str]'             );
 has dist    => ( ro, isa=>'Maybe[Str]', lazy_build );
-has pkgname => ( ro, isa=>'Str', required          );
+has pkgname => ( ro, isa=>'Str',        required   );
 
 
 # -- initializers & builders
@@ -72,3 +72,4 @@ available modules.
 Note that for C<dist> to return a meaningful result, it needs the
 L<CPANPLUS> index, which should exist if you already used CPANPLUS at
 least once.
+
