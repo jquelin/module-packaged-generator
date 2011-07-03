4575d62e2a93da1c23e6311e1334cb30dfd0ca78
Jérôme Quelin <jquelin@gmail.com>
2011-07-03 10:24:13 +0200

    moose role for easy logging

---
+++ lib/Module/Packaged/Generator/Role/Loggable.pm
@@ -0,0 +1,51 @@
+use 5.012;
+use strict;
+use warnings;
+
+package Module::Packaged::Generator::Role::Loggable;
+# ABSTRACT: role to provide easy logging
+
+use Moose::Role;
+use MooseX::Has::Sugar;
+
+use Module::Packaged::Generator::Logger;
+
+# -- private attributes
+
+has _logger => (
+    ro,
+    isa     => 'Module::Packaged::Generator::Logger',
+    default => sub { Module::Packaged::Generator::Logger->instance },
+    handles => [ qw{
+        log log_fatal log_debug
+        set_debug set_muted progress_bar
+    } ],
+);
+
+=method log
+
+=method log_fatal
+
+=method log_debug
+
+=method set_debug
+
+=method set_muted
+
+=method progress_bar
+
+Those methods are imported from L<Module::Packaged::Generator::Logger> -
+refer to this module for more information.
+
+=cut
+
+no Moose::Role;
+
+1;
+__END__
+
+=head1 DESCRIPTION
+
+This L<Moose> role provides the consuming class with an easy access to
+L<Module::Packaged::Generator::Logger>.
+
